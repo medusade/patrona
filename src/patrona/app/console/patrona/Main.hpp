a448f92594d9c0f8257573140aea0c8745309e14
medusade <medusaxde@gmail.com>
2018-12-23 09:18:35 -0500

    Updated source.

---
+++ src/patrona/app/console/patrona/Main.hpp
@@ -0,0 +1,75 @@
+///////////////////////////////////////////////////////////////////////
+/// Copyright (c) 1988-2018 $organization$
+///
+/// This software is provided by the author and contributors ``as is''
+/// and any express or implied warranties, including, but not limited to,
+/// the implied warranties of merchantability and fitness for a particular
+/// purpose are disclaimed. In no event shall the author or contributors
+/// be liable for any direct, indirect, incidental, special, exemplary,
+/// or consequential damages (including, but not limited to, procurement
+/// of substitute goods or services; loss of use, data, or profits; or
+/// business interruption) however caused and on any theory of liability,
+/// whether in contract, strict liability, or tort (including negligence
+/// or otherwise) arising in any way out of the use of this software,
+/// even if advised of the possibility of such damage.
+///
+///   File: Main.hpp
+///
+/// Author: $author$
+///   Date: 12/22/2018
+///////////////////////////////////////////////////////////////////////
+#ifndef _PATRONA_APP_CONSOLE_PATRONA_MAIN_HPP
+#define _PATRONA_APP_CONSOLE_PATRONA_MAIN_HPP
+
+#include "patrona/console/getopt/Main.hpp"
+#include "patrona/lib/patrona/Version.hpp"
+
+namespace patrona {
+namespace app {
+namespace console {
+namespace patrona {
+
+typedef ::patrona::console::getopt::Main::Implements MainTImplements;
+typedef ::patrona::console::getopt::Main MainTExtends;
+///////////////////////////////////////////////////////////////////////
+///  Class: MainT
+///////////////////////////////////////////////////////////////////////
+template <class TImplements = MainTImplements, class TExtends = MainTExtends>
+class _EXPORT_CLASS MainT: virtual public TImplements, public TExtends {
+public:
+    typedef TImplements Implements;
+    typedef TExtends Extends;
+
+    typedef typename Extends::char_t char_t;
+    typedef typename Extends::endchar_t endchar_t;
+    static const endchar_t endchar = Extends::endchar;
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    MainT() {
+    }
+    virtual ~MainT() {
+    }
+
+protected:
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    virtual int Run(int argc, char_t**argv, char_t** env) {
+        const ::patrona::lib::Version& version = ::patrona::lib::patrona::Version::Which();
+        int err = 0;
+        this->OutL(version.Name(), " version = ", version.ToString().chars(), NULL);
+        this->OutLn();
+        return err;
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+}; /// class _EXPORT_CLASS MainT
+typedef MainT<> Main;
+
+} /// namespace patrona
+} /// namespace console
+} /// namespace app
+} /// namespace patrona
+
+#endif /// ndef _PATRONA_APP_CONSOLE_PATRONA_MAIN_HPP
---
+++ src/patrona/base/Base.hpp
@@ -0,0 +1,112 @@
+///////////////////////////////////////////////////////////////////////
+/// Copyright (c) 1988-2018 $organization$
+///
+/// This software is provided by the author and contributors ``as is'' 
+/// and any express or implied warranties, including, but not limited to, 
+/// the implied warranties of merchantability and fitness for a particular 
+/// purpose are disclaimed. In no event shall the author or contributors 
+/// be liable for any direct, indirect, incidental, special, exemplary, 
+/// or consequential damages (including, but not limited to, procurement 
+/// of substitute goods or services; loss of use, data, or profits; or 
+/// business interruption) however caused and on any theory of liability, 
+/// whether in contract, strict liability, or tort (including negligence 
+/// or otherwise) arising in any way out of the use of this software, 
+/// even if advised of the possibility of such damage.
+///
+///   File: Base.hpp
+///
+/// Author: $author$
+///   Date: 12/22/2018
+///////////////////////////////////////////////////////////////////////
+#ifndef _PATRONA_BASE_BASE_HPP
+#define _PATRONA_BASE_BASE_HPP
+
+#include "nadir/base/base.hpp"
+#include "nadir/base/array.hpp"
+#include "nadir/base/string.hpp"
+#include "nadir/base/to_string.hpp"
+
+namespace patrona {
+
+typedef ::nadir::pointer_t pointer_t;
+typedef ::nadir::unsigned_t unsigned_t;
+typedef ::nadir::signed_t signed_t;
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+typedef ::nadir::implement_base implement_base;
+typedef ::nadir::base base;
+
+typedef ::nadir::char_array char_array;
+typedef ::nadir::tchar_array tchar_array;
+typedef ::nadir::wchar_array wchar_array;
+
+typedef ::nadir::char_string char_string;
+typedef ::nadir::tchar_string tchar_string;
+typedef ::nadir::wchar_string wchar_string;
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+typedef ::nadir::bool_to_string bool_to_string;
+typedef ::nadir::bool_to_tstring bool_to_tstring;
+typedef ::nadir::bool_to_wstring bool_to_wstring;
+
+typedef ::nadir::chars_to_string chars_to_string;
+typedef ::nadir::chars_to_tstring chars_to_tstring;
+typedef ::nadir::chars_to_wstring chars_to_wstring;
+
+typedef ::nadir::int_to_string int_to_string;
+typedef ::nadir::int_to_tstring int_to_tstring;
+typedef ::nadir::int_to_wstring int_to_wstring;
+
+typedef ::nadir::signed_to_string signed_to_string;
+typedef ::nadir::signed_to_tstring signed_to_tstring;
+typedef ::nadir::signed_to_wstring signed_to_wstring;
+
+typedef ::nadir::unsigned_to_string unsigned_to_string;
+typedef ::nadir::unsigned_to_tstring unsigned_to_tstring;
+typedef ::nadir::unsigned_to_wstring unsigned_to_wstring;
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+inline const pointer_t& to_pointer(const pointer_t& v) { return v; }
+inline const unsigned_t& to_unsigned(const unsigned_t& v) { return v; }
+inline const signed_t& to_signed(const signed_t& v) { return v; }
+inline const wchar_t& to_wchar(const wchar_t& v) { return v; }
+inline const char& to_char(const char& v) { return v; }
+inline const bool& to_bool(const bool& v) { return v; }
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+inline seconds_t mseconds_seconds
+(mseconds_t mseconds) { return nadir::mseconds_seconds(mseconds); }
+
+inline mseconds_t mseconds_mseconds
+(mseconds_t mseconds) { return nadir::mseconds_mseconds(mseconds); }
+
+inline useconds_t mseconds_useconds
+(mseconds_t mseconds) { return nadir::mseconds_useconds(mseconds); }
+
+inline nseconds_t mseconds_nseconds
+(mseconds_t mseconds) { return nadir::mseconds_nseconds(mseconds); }
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+typedef implement_base ImplementBase;
+typedef base Base;
+
+typedef char_array CharArray;
+typedef tchar_array TCharArray;
+typedef wchar_array WCharArray;
+
+typedef char_string String;
+typedef tchar_string TString;
+typedef wchar_string WString;
+
+typedef chars_to_string CharsToString;
+typedef chars_to_tstring CharsToTString;
+typedef chars_to_wstring CharsToWString;
+
+} // namespace patrona
+
+#endif // _PATRONA_BASE_BASE_HPP
